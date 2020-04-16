bd5f9bb0065f51d7a497fe111bc88ec89711436a
Flakari <natedimock@yahoo.com>
2020-04-16 12:38:11 -0500

    Updates

---
+++ precache-manifest.16add942baa41c3be93a633d406100bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d089112e631491983f1bb2cee8746982",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e04d413ac1cd31ce030b",
+    "url": "/portfolio/static/css/main.a26f8a14.chunk.css"
+  },
+  {
+    "revision": "da995585c1ae3d5345f5",
+    "url": "/portfolio/static/js/2.cda4e980.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.cda4e980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04d413ac1cd31ce030b",
+    "url": "/portfolio/static/js/main.bc333aa4.chunk.js"
+  },
+  {
+    "revision": "4a389105ba556f07fe48",
+    "url": "/portfolio/static/js/runtime-main.43337fc2.js"
+  },
+  {
+    "revision": "4b1d35e1d6cdf3672dfbe1dbe42c072f",
+    "url": "/portfolio/static/media/brackets.4b1d35e1.png"
+  },
+  {
+    "revision": "dfe70190ca959daba0977933aedd72c6",
+    "url": "/portfolio/static/media/wallpaper.dfe70190.jpg"
+  }
+]);
