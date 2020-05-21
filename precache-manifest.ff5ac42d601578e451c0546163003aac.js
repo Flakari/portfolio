91bf02f741c61dd9c5955d1a0dc0eb89f891bcd6
Flakari <natedimock@yahoo.com>
2020-05-21 12:07:33 -0500

    Updates

--- precache-manifest.ff5ac42d601578e451c0546163003aac.js
+++ precache-manifest.ff5ac42d601578e451c0546163003aac.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8aeae2cd06f6a8d17e6fa29f5f8fb9b",
+    "revision": "5849c343328683ffe75941087817415c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "37c2006dbed16702e8d8",
-    "url": "/portfolio/static/css/main.b3700ffc.chunk.css"
+    "revision": "c65cac7813ecfbadc6b5",
+    "url": "/portfolio/static/css/main.1e562355.chunk.css"
   },
   {
     "revision": "7980054b25ba0df9c224",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.80ce03cb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "37c2006dbed16702e8d8",
-    "url": "/portfolio/static/js/main.9a2ece07.chunk.js"
+    "revision": "c65cac7813ecfbadc6b5",
+    "url": "/portfolio/static/js/main.0fba6f76.chunk.js"
   },
   {
     "revision": "4a389105ba556f07fe48",
@@ -28,16 +28,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/Adobe-PDF-File-Icon-01.1012a2bd.svg"
   },
   {
-    "revision": "90aeb663267e0b5938ef2263cd8a044e",
-    "url": "/portfolio/static/media/Codepen-logo.90aeb663.png"
+    "revision": "26ab4e0566da5a89c9c6d5f589abf4b9",
+    "url": "/portfolio/static/media/Codepen-logo.26ab4e05.png"
   },
   {
     "revision": "7b576b125ef321522393226676becf47",
     "url": "/portfolio/static/media/Figma-logo.7b576b12.svg"
   },
   {
-    "revision": "19b70602a36ea20daa3182c77777b904",
-    "url": "/portfolio/static/media/Game-of-Life.19b70602.png"
+    "revision": "2239381ee4a63453013f4a7fd32d7995",
+    "url": "/portfolio/static/media/Game-of-Life.2239381e.png"
   },
   {
     "revision": "818b2ae6d52712fe3f862193d3c84577",
@@ -48,24 +48,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/Nathan Dimock Resume.7ef4546f.pdf"
   },
   {
-    "revision": "4c2db1e1b35117d6306106dfa1f8d144",
-    "url": "/portfolio/static/media/Pomodoro-Clock.4c2db1e1.png"
+    "revision": "e31c26a213082f88a48d0b598ed233f8",
+    "url": "/portfolio/static/media/Pomodoro-Clock.e31c26a2.png"
   },
   {
-    "revision": "61175183d7ef98fca9dc9b21d8d2521e",
-    "url": "/portfolio/static/media/VG-Lists.61175183.png"
+    "revision": "e1f8d997df6d145212abc4878ebf29a6",
+    "url": "/portfolio/static/media/VG-Lists.e1f8d997.png"
   },
   {
     "revision": "e00a35696e2159961e17ae487576275e",
     "url": "/portfolio/static/media/Visual_Studio_Code_1.35_icon.e00a3569.svg"
   },
   {
-    "revision": "7182f7f00035eeaf16e3ca9ca0936701",
-    "url": "/portfolio/static/media/Weather-App.7182f7f0.png"
+    "revision": "e9967e8c4105b76afb3ef2d5149feb16",
+    "url": "/portfolio/static/media/Weather-App.e9967e8c.png"
   },
   {
-    "revision": "4b1d35e1d6cdf3672dfbe1dbe42c072f",
-    "url": "/portfolio/static/media/brackets.4b1d35e1.png"
+    "revision": "88a08cca45f5bcde45b6fe37d2e2ece4",
+    "url": "/portfolio/static/media/brackets.88a08cca.png"
   },
   {
     "revision": "7cd00d41e02c8c3313d80af17810d978",
@@ -108,7 +108,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/segoeuisl.426088e4.ttf"
   },
   {
-    "revision": "dfe70190ca959daba0977933aedd72c6",
-    "url": "/portfolio/static/media/wallpaper.dfe70190.jpg"
+    "revision": "605b63f742c57f50b48c9c6867d9c89e",
+    "url": "/portfolio/static/media/wallpaper.605b63f7.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.074a993c3cc8a947ed5e21cfec861c37.js"
+  "/portfolio/precache-manifest.ff5ac42d601578e451c0546163003aac.js"
 );
 
 self.addEventListener('message', (event) => {
