1d41edcd76ba5b95ffd3457b1c1938830a0f511b
Flakari <natedimock@yahoo.com>
2020-12-02 19:00:52 -0600

    Replaced custom persistant nav styling functionality with react-router built in solution

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,45 +1,22 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = (props) => {
-    const [currentPage, setCurrentPage] = useState(String(document.URL.match(/\w+?$/)));
-
-    const changeHighlight = value => {
-        setCurrentPage(value);
-    }
-
     return (
         <nav className={props.show ? 'visible' : null}>
             <ul>
-                <Link to="/">
-                    <li 
-                        className={currentPage === 'null' ? 'highlight' : null} 
-                        onClick={() => {changeHighlight('null'); props.click();}}
-                    >
-                        Home
-                    </li>
-                </Link>
-                <Link to="/about">
-                    <li 
-                        className={currentPage === 'about' ? 'highlight' : null} 
-                        onClick={() => {changeHighlight('about'); props.click();}}>
-                        About
-                    </li>
-                </Link>
-                <Link to="/projects">
-                    <li 
-                        className={currentPage === 'projects' ? 'highlight' : null} 
-                        onClick={() => {changeHighlight('projects'); props.click();}}>
-                        Projects
-                    </li>
-                </Link>
-                <Link to="/contact">
-                    <li 
-                        className={currentPage === 'contact' ? 'highlight' : null} 
-                        onClick={() => {changeHighlight('contact'); props.click();}}>
-                        Contact
-                    </li>
-                </Link>
+                <NavLink to="/" exact>
+                    <li onClick={props.click}>Home</li>
+                </NavLink>
+                <NavLink to="/about">
+                    <li onClick={props.click}>About</li>
+                </NavLink>
+                <NavLink to="/projects">
+                    <li onClick={props.click}>Projects</li>
+                </NavLink>
+                <NavLink to="/contact">
+                    <li onClick={props.click}>Contact</li>
+                </NavLink>
             </ul>
         </nav>
     )
